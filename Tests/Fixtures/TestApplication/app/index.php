3f41c2de1ab3a2b73a63efa638cc93317f9bb4d0
Jeanmonod David <david.jeanmonod@gmail.com>
2013-09-11 20:11:01 +0200

    Add a front controller to ease test debugging

---
+++ Tests/Fixtures/TestApplication/app/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Because debugging in the browser is much easier than in phpunit...
+
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../../../bootstrap.php';
+require_once __DIR__.'/AppKernel.php';
+$kernel = new AppKernel('test', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
