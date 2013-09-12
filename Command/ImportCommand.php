9b880544829a52887b5079b78922a695a6d9477e
Jeanmonod David <david.jeanmonod@gmail.com>
2013-09-13 01:27:08 +0200

    Add a new command to import existing translations

---
+++ Command/ImportCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Liip\TranslationBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Liip\TranslationBundle\Translation\DualModeTranslator;
+use Liip\TranslationBundle\Manager\Manager;
+use Symfony\Component\Translation\Translator;
+
+
+/**
+ * Class ImportCommand
+ *
+ * Import all existing translations into the current translation storage
+ *
+ * @package Liip\TranslationBundle\Command
+ */
+class ImportCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('translation:import')
+            ->setDescription('Import all existing translations into the application storage')
+            ->setDefinition(array(
+                new InputOption('locales', null, InputOption::VALUE_REQUIRED, 'A comma separated list of locales ( --locales=en,fr,fr_CH')
+            ))
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $importOptions = array();
+        $importOptions['logger'] = $output;
+        if ($locales = $input->getOption('locales')) {
+            $importOptions['locale_list'] = explode(',', $locales);
+        }
+
+        /** @var Manager $translationManager */
+        $translationManager =  $this->getContainer()->get('liip.translation.manager');
+        $translationManager->processImportOfStandardResources($importOptions);
+    }
+
+}
--- Manager/Manager.php
+++ Manager/Manager.php
@@ -37,4 +37,60 @@ class Manager
         return $this->translator->getStandardResources();
     }
 
+    /**
+     * Return true if a resource must be ignore
+     *
+     * @return boolean
+     */
+    public function checkIfResourceIsIgnored($resource)
+    {
+        return false; // TODO
+    }
+
+    public function processImportOfStandardResources($options)
+    {
+        if (array_key_exists('logger', $options)){
+            $this->logger = $options['logger'];
+        }
+
+        $locales = array_key_exists('locale_list', $options) ? $options['locale_list'] : $this->getLocaleList();
+
+        $this->log("<info>Start importation for locales:</info> [".implode(', ', $locales)."]\n");
+
+        foreach($this->getStandardResources() as $resource) {
+
+            $this->log("Import resource <info>{$resource['path']}</info>");
+
+            if ($this->checkIfResourceIsIgnored($resource)) {
+                $this->log("  >> <comment>Skipped</comment> (due to ignore settings from the config)\n");
+                continue;
+            }
+
+            if (!in_array($resource['locale'], $locales)) {
+                $this->log("  >> <comment>Skipped</comment> (unwanted locales)\n");
+                continue;
+            }
+
+            /** @var MessageCatalogue $catalog */
+            $catalog = $this->translator->loadResource($resource);
+
+            foreach ($catalog->all() as $domain => $translations) {
+                $this->log("\n  Import catalog <comment>$domain</comment>\n");
+                foreach($translations as $key => $value) {
+                    $this->log("    >> new key [$key] with a base value of [$value]\n");
+                    $metadata = $catalog->getMetadata($key, $domain);
+                    var_dump($metadata);
+                    $this->storage->import($domain, $key, $resource['locale'], $metadata, $value);
+                }
+            }
+            $this->log(" <info>Import success</info>\n");
+
+        }
+    }
+
+    protected function log($msg) {
+        if ($this->logger) {
+            $this->logger->write($msg);
+        }
+    }
 }
