4799d07f24ac498996e1c7e4ce9a55253a4ae7f1
Gilles Meier <gilles.meier@liip.ch>
2013-09-23 19:55:00 +0000

    fix : set the umask for cache creation

--- Tests/Fixtures/TestApplication/app/AppKernel.php
+++ Tests/Fixtures/TestApplication/app/AppKernel.php
@@ -3,6 +3,8 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+umask(0000);
+
 class AppKernel extends Kernel
 {
     public function registerBundles()
