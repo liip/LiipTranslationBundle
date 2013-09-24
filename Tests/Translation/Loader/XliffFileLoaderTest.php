8a379dd16dccbf9cca547069683a4945bf939e5c
Jeanmonod David <david.jeanmonod@gmail.com>
2013-09-24 22:48:47 +0200

    Rename elf file to cliff to be compliant with sf2.0

--- Tests/Translation/Loader/XliffFileLoaderTest.php
+++ Tests/Translation/Loader/XliffFileLoaderTest.php
@@ -23,7 +23,7 @@ class XliffFileLoaderTest extends \PHPUnit_Framework_TestCase
     public function testMetaDataExtraction()
     {
         $loader = new XliffFileLoader();
-        $catalogue = $loader->load(__DIR__.'/../../Fixtures/test.xlf', 'en', 'messages');
+        $catalogue = $loader->load(__DIR__.'/../../Fixtures/test.xliff', 'en', 'messages');
 
         $this->assertEquals(array('id'=>1), $catalogue->getMetadata('without.metadata'));
         $this->assertEquals(array('id'=>2, 'minbytes'=>'10', 'maxbytes'=>'100'), $catalogue->getMetadata('with.metadata'));
@@ -32,7 +32,7 @@ class XliffFileLoaderTest extends \PHPUnit_Framework_TestCase
     public function testNoteExtraction()
     {
         $loader = new XliffFileLoader();
-        $catalogue = $loader->load(__DIR__.'/../../Fixtures/test.xlf', 'en', 'messages');
+        $catalogue = $loader->load(__DIR__.'/../../Fixtures/test.xliff', 'en', 'messages');
 
         $this->assertEquals(array('id'=>3, 'note'=>'Please edit it carefully'), $catalogue->getMetadata('with.note'));
     }
