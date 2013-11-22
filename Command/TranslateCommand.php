0018aa71c5e5a68701e9ef576659b6eb0db62eaa
Jeanmonod David <david.jeanmonod@gmail.com>
2013-11-22 09:25:43 +0100

    Fix the default domain in the translate commmand

--- Command/TranslateCommand.php
+++ Command/TranslateCommand.php
@@ -35,7 +35,7 @@ class TranslateCommand extends ContainerAwareCommand
             ->setDefinition(array(
                 new InputArgument('key', InputArgument::REQUIRED, 'The key to translate'),
                 new InputOption('locale', 'l', InputOption::VALUE_REQUIRED, 'Destination locale', 'en'),
-                new InputOption('domain', 'd', InputOption::VALUE_REQUIRED, 'Message domain', 'message')
+                new InputOption('domain', 'd', InputOption::VALUE_REQUIRED, 'Message domain', 'messages')
             ))
         ;
     }
