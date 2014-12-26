0bcc8a51e240a27d2231031ee1957d08e30d26f1
Lukas Kahwe Smith <smith@pooteeweet.org>
2014-12-26 16:40:31 +0100

    fix overwriting the default translator service

---
+++ DependencyInjection/Compiler/TranslatorPass.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Liip\TranslationBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Compiler pass to update the liip.translation.translator
+ *
+ * This file is part of the LiipTranslationBundle. For more information concerning
+ * the bundle, see the README.md file at the project root.
+ *
+ * @package Liip\TranslationBundle\DependencyInjection
+ * @version 0.1.0
+ *
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @author Lukas Kahwe Smith <smith@pooteeweet.org>
+ * @copyright Copyright (c) 2015, Liip, http://www.liip.ch
+ */
+class TranslatorPass implements CompilerPassInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $container->setAlias('translator', 'liip.translation.translator');
+    }
+}
