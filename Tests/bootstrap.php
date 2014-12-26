5fb79f709f5e56b3f58498ed65fc0991f3b383e7
David Buchmann <david.buchmann@liip.ch>
2014-12-26 12:41:23 +0100

    testing symfony 2.7 and adjust notification

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL ^ E_USER_DEPRECATED);
 if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     throw new RuntimeException("Install dependencies using composer to run the test suite.");
 }
